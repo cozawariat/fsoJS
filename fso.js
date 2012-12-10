3738af8f01e1f8649769a57284cd7fffa6935f6a
amaru <cozawariat@gmail.com>
2012-12-10 20:56:45 +0100

    rewrite

--- fso.js
+++ fso.js
@@ -1,74 +1,52 @@
 var fso = {
-    name: null,
-    f: new ActiveXObject("Scripting.FileSystemObject"),
-    temp: null,
-    txt: {
-        file: null,
-        io: null,
-        created: false,
+    a: new ActiveXObject("Scripting.FileSystemObject"),
+    n: '',
+    t: '',
+	p: {},
+    x: {
+        f: 	null,
+        o: 	0,
         read: function () {
-            var size = fso.f.getFile(fso.name).size;
-            if ( !! this.file && size && this.io === 1) {
-                var ret = this.file.ReadAll();
-                this.file.Close();
-                return ret;
-            } else {
-                if (!this.file || !size) return '';
-                fso.open(fso.name).read();
-            }
+			if ( this.o ) {
+				return fso.a.GetFile(fso.n).size ? this.f.ReadAll() : '';
+			}
+			else {
+				this.o = 1;
+				return fso.open(0,{mode:1}).read();
+			}
         },
-        write: function (str) {
-            if (!this.file || this.io !== 2) return fso.open(fso.name, {
-                mode: 2
-            }).write(str);
-            else {
-                this.file.Write(str);
-                return fso.open(fso.name).read();
-            }
+        write: function (q,d) {
+			if ( this.o ) {
+				this.f.Write(q);
+				this.o = 0;
+				return this;
+			}
+			else {
+				this.o = 1;
+				return fso.open(0,{mode:d||2}).write(q);
+			}
+		},
+        append: function (q) {
+			return this.write(q,8);
         },
-        append: function (str) {
-            if (!this.file || this.io !== 8) {
-                return fso.open(fso.name, {
-                    mode: 8
-                }).append(str);
-            } else {
-                this.file.Write(str);
-                return fso.open(fso.name).read();
-            }
-        },
-        prepend: function (str) {
-            if (!this.file || this.io !== 2) {
-                fso.temp = this.read();
-                return fso.open(fso.name, {
-                    mode: 2
-                }).prepend(str);
-            } else {
-                this.file.Write(str + fso.temp);
-                return fso.open(fso.name).read();
-            }
+        prepend: function (q) {
+			fso.t = this.read();
+			return fso.open(0,{mode:2}).write(q+fso.t);
         }
     },
-    create: function (fileName) {
-        if (fso.f.exists(fileName)) return false;
-        return fso.open(fileName, {
-            create: true
-        });
-    },
-    open: function (fileName, params) {
-        params = params || {};
-        params = {
-            mode: params.mode || 1,
-            create: params.create || true,
-            format: params.format || 0
-        };
-        fso.txt.io = params.mode;
-        fso.txt.created = !fso.exists(fileName);
-        if (fso.txt.file) fso.txt.file.Close();
-        fso.txt.file = fso.f.OpenTextFile(fileName, params.mode, params.create, params.format);
-        fso.name = fileName;
-        return fso.txt;
-    },
-    exists: function (fileName) {
-        return fso.f.FileExists(fileName) || fso.f.FolderExists(fileName);
+    open: function (z,b) {
+		fso.n = fso.n || z;
+		if ( !fso.a.FileExists(fso.n) ) fso.a.CreateTextFile(fso.n,false);
+		if ( fso.x.o ) {
+			b = b || {};
+			b = {
+				mode: b.mode || 1,
+				create: b.create || 1,
+				format: b.format || 0
+			};
+			fso.p = b;
+			fso.x.f = fso.a.OpenTextFile(fso.n, fso.p.mode, fso.p.create, fso.p.format);
+		}
+		return fso.x;
     }
 };
